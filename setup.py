d1974cc7272630d0f5ac3dc0b43ee358744644c6
Mostafa Atallah <m.atallah.2014@gmail.com>
2025-06-04 12:09:11 -0400

    Fix setuo issues

--- setup.py
+++ setup.py
@@ -1,12 +1,18 @@
 #!/usr/bin/env python3
 
 from setuptools import setup, find_packages
+
 setup(
-    name = 'qubap',
-    packages = find_packages(),
-    install_requires = [
-        "numpy",
-        "qiskit",
+    name='qubap',
+    packages=find_packages(),
+    install_requires=[
+        "qiskit[visualization]>=0.46,<1.0",
         "qiskit_aer",
+        "numpy>=1.17,<3.0",
+        "scipy>=1.5",
+        "matplotlib>=3.3",
+        "pylatexenc",
+        "ipywidgets",        
+        "jupyter",           
     ],
 )
